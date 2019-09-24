b679d5ce99aaa642ec819c8c0609dc8a13ae86ee
Klaabut <54986407+Klaabut@users.noreply.github.com>
2019-09-24 14:32:32 +0300

    Kodutöövalmis

--- BMI/Program.cs
+++ BMI/Program.cs
@@ -13,15 +13,15 @@ namespace BMI
 
             Console.WriteLine("Mis aastal sa sündisid?");
             int yob;
-            int h;
-            int w;
+            double h;
+            double w;
 
             yob = int.Parse(Console.ReadLine());
 
             if (yob < 1959)
             {
 
-                Console.WriteLine("ERROR oled liiga vana :D");
+                Console.WriteLine("Programm annab õigeid tulemusi vanusevahemikus 20-60. Teie puhul ei oleks tulemus õige.");
 
             }
             else
@@ -36,14 +36,53 @@ namespace BMI
                 w = int.Parse(Console.ReadLine());
 
                 // BMI arvutus
-                double BMI = w / (h * h) ;
+               
+                double BMI = w / ((h * h) / 10000) ;
                 
                 
 
-                Console.WriteLine($"Sinu kehamassiindeks on {Math.Round(BMI, 5)}  ");
+                Console.WriteLine($"Sinu kehamassiindeks on " + BMI );
 
-                Console.ReadLine();
+                if (BMI < 16)
+                {
+                    Console.WriteLine("Te olete tervisele ohtlikus alakaalus.");
+                }
+
+
+                else if (BMI >= 16 && BMI <= 18.5)
+                {
+                    Console.WriteLine("Te olete alakaalus.");
+                }
+
+                else if (BMI >= 18.6 && BMI <= 25)
+                {
+                    Console.WriteLine("Te olete Normaalkaalus.");
+                }
+
+                else if (BMI >= 25.1 && BMI <= 30)
+                {
+                    Console.WriteLine("Te olete ülekaalus.");
+                }
+
+                else if (BMI >= 30.1 && BMI <= 35)
+                {
+                    Console.WriteLine("Te olete rasvunud.");
+                }
+
+                else if (BMI >= 35.1 && BMI <= 40)
+                {
+                    Console.WriteLine("Te olete tugevalt rasvunud.");
+                }
+
+                else
+                {
+                    Console.WriteLine("Te olete tervisele ohtlikult rasvunud.");
+                }
+
+                
             }
+                Console.ReadLine();
         }
+        
     }
 }
